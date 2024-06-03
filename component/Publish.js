400099b6f2fe4fc1d08acf33f9cfb7d6bff76d1e
Barros313 <gbarros1337@gmail.com>
2024-06-03 16:47:53 -0300

    Added style component and header text

--- component/Publish.js
+++ component/Publish.js
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function Publish() {
     return (
         <View>
-            <Text> Publish </Text>
+            <PublishHeader/>
         </View>
     );
 }
+
+function PublishHeader() {
+    return (
+        <View style={styles.header}>
+            <Text> Anunciar </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: 'blue',
+    }
+});
