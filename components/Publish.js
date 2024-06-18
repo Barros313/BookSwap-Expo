9e8c9a7b30344b44867768221de543c5ea63fbb8
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-18 10:33:16 -0300

    Refactored ImageSelect and added simple form

--- components/Publish.js
+++ components/Publish.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageSelect from "./api/ImageSelect";
+import ImageSelect from "./forms/image-api/ImageSelect";
 import BookForm from "./forms/BookForm";
 
 export default function Publish() {
