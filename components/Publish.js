92767c7f9054f36bd8357098c47a27fab857c82c
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-18 08:58:01 -0300

    Separated functions in different folders

--- components/Publish.js
+++ components/Publish.js
@@ -1,64 +1,12 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-
-import * as ImagePicker from 'expo-image-picker';
-
+import React from "react";
+import ImageSelect from "./api/ImageSelect";
+import BookForm from "./forms/BookForm";
 
 export default function Publish() {
     return (
         <>
-            <ImageContainer />
+            <ImageSelect />
             <BookForm />
         </>
     );
 }
-
-const BookForm = () => {
-    return (
-        <></>
-    );
-};
-
-function ImageContainer() {
-    const [ image, setImage ] = useState(null);
-
-    const pickImage = async() => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [4, 4],
-            quality: 1
-        });
-
-        console.log(result);
-
-        if (!result.canceled) {
-            setImage(result.assets[0].uri);
-        };
-    };
-
-    return (
-        <View style={imageStyle.container}>
-            { image && <Image source={{ uri: image }} style={imageStyle.image} /> }
-
-            <Pressable onPress={pickImage}>
-                <Ionicons name="images-sharp" size={20} />
-                <Text> Selecionar foto </Text>
-            </Pressable>
-
-        </View>
-    );
-}
-
-const imageStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image: {
-        width: 200,
-        height: 200
-    }
-});
