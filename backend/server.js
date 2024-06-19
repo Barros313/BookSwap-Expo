50e792f09827adb22d4a813a5ba80659e96f8220
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-19 11:14:05 -0300

    Added get request to output every user registered in the system

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/authDB', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected!'))
