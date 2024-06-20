a0fd22380ea97def86ff710d1d4314a1c1db2483
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-20 19:13:18 -0300

    Added styling to login and register pages

--- frontend/components/authentication/Login.js
+++ frontend/components/authentication/Login.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import { View, TextInput, Button } from 'react-native';
+import {View, TextInput, Button, StyleSheet, Image} from 'react-native';
 import axios from 'axios';
 import {UserContext} from "./UserContext";
 
@@ -24,13 +24,43 @@ export default function Login({ route, navigation }) {
     };
 
     return (
-      <View>
-          <TextInput placeholder="Email" onChangeText={setEmail} value={email} />
-          <TextInput placeholder="Senha" onChangeText={setPassword} secureTextEntry value={password} />
+      <View style={styles.container}>
+          <Image style={styles.logo} source={{ uri: 'https://i.ibb.co/02QLTG2/applogo-min.png' }} />
+
+          <TextInput style={styles.input} placeholder="Email" onChangeText={setEmail} value={email} />
+          <TextInput style={styles.input} placeholder="Senha" onChangeText={setPassword} secureTextEntry value={password} />
           <Button title='Entrar' onPress={handleLogin} />
 
-          <Button title='Primeiro acesso? Cadastre-se' onPress={() => navigation.navigate('Register')} />
+          <Button style={styles.button} title='Primeiro acesso? Cadastre-se' onPress={() => navigation.navigate('Register')} />
 
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'gray',
+        margin: 20,
+        paddingHorizontal: 10,
+    },
+    logo : {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        marginBottom: 20,
+        borderWidth: 2,
+        borderColor: '#000',
+    },
+    button: {
+        margin: 20,
+    }
+});
--- frontend/components/authentication/Register.js
+++ frontend/components/authentication/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button } from "react-native";
+import {View, TextInput, Button, StyleSheet, Image} from "react-native";
 import axios from 'axios'
 
 export default function Register({ navigation }) {
@@ -20,14 +20,45 @@ export default function Register({ navigation }) {
     };
 
     return (
-        <View>
-            <TextInput placeholder="Nome de Usuário" onChangeText={setUsername} value={username} />
-            <TextInput placeholder="Email" onChangeText={setEmail} value={email} />
-            <TextInput placeholder="Senha" onChangeText={setPassword} value={password} secureTextEntry />
+        <View style={styles.container} >
+            <Image style={styles.logo} source={{ uri: 'https://i.ibb.co/02QLTG2/applogo-min.png' }} />
+
+            <TextInput style={styles.input} placeholder="Nome de Usuário" onChangeText={setUsername} value={username} />
+            <TextInput style={styles.input} placeholder="Email" onChangeText={setEmail} value={email} />
+            <TextInput style={styles.input} placeholder="Senha" onChangeText={setPassword} value={password} secureTextEntry />
             <Button title="Cadastrar" onPress={handleRegister} />
 
-            <Button title="Já possui uma conta? Faça login" onPress={() => navigation.navigate("Login")} />
+            <Button style={styles.button} title="Já possui uma conta? Faça login" onPress={() => navigation.navigate("Login")} />
 
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'gray',
+        margin: 20,
+        paddingHorizontal: 10,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        marginBottom: 20,
+        borderWidth: 2,
+        borderColor: '#000',
+    },
+    button: {
+        margin: 20,
+    }
+});
