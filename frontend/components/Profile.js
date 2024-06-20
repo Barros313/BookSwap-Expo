91fce1bedb322a6b335868657851473a5a38e747
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-20 04:30:55 -0300

    Profile template

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import {View, Image, Text} from "react-native";
 import React from "react";
 
 export default function Profile() {
     return (
         <View>
-            <Text> Profile </Text>
+            <Image source={require('../assets/default-user-image-temp.jpg')} />
+            <Text> username </Text>
+            <Text> email </Text>
+
         </View>
     );
 }
